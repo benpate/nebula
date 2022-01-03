3d22949ae9ffc64a1a792d0b3fb325e38ef1eec5
Ben Pate <ben@pate.org>
2022-01-03 10:13:25 -0700

    Add item split tests

--- action_add_cols_test.go
+++ action_add_cols_test.go
@@ -43,10 +43,19 @@ func TestAddItem_Columns_RIGHT(t *testing.T) {
 
 		require.Nil(t, err)
 		require.Equal(t, 3, len(container))
-		require.Equal(t, "LAYOUT", container.GetType(0))
-		require.Equal(t, []int{1, 2}, container.GetRefs(0))
-		require.Equal(t, "HTML", container.GetType(1))
-		require.Equal(t, "TEXT", container.GetType(2))
+		{
+			item := container.GetItem(0)
+			require.Equal(t, "LAYOUT", item.Type)
+			require.Equal(t, []int{1, 2}, item.Refs)
+		}
+		{
+			item := container.GetItem(1)
+			require.Equal(t, "HTML", item.Type)
+		}
+		{
+			item := container.GetItem(2)
+			require.Equal(t, "TEXT", item.Type)
+		}
 	}
 
 	{
--- action_add_item_test.go
+++ action_add_item_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestItem(t *testing.T) {
+func TestItem_Split_ABOVE(t *testing.T) {
 
 	library := NewLibrary()
 
@@ -17,7 +17,121 @@ func TestItem(t *testing.T) {
 
 	itemID, err := container.Execute(&library, datatype.Map{
 		"type":     "add-item",
-		"itemId":   2,
+		"itemId":   0,
+		"itemType": "WYSIWYG",
+		"place":    "ABOVE",
+		"check":    container.GetChecksum(firstID),
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "ROWS", item.GetString("style"))
+		require.Equal(t, []int{1, 2}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
+}
+
+func TestItem_Split_BELOW(t *testing.T) {
+
+	library := NewLibrary()
+
+	container := NewContainer()
+
+	firstID := container.NewItemWithInit(&library, ItemTypeHTML, datatype.Map{"html": "FIRST HTML ITEM"})
+
+	itemID, err := container.Execute(&library, datatype.Map{
+		"type":     "add-item",
+		"itemId":   0,
+		"itemType": "WYSIWYG",
+		"place":    "BELOW",
+		"check":    container.GetChecksum(firstID),
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "ROWS", item.GetString("style"))
+		require.Equal(t, []int{2, 1}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
+}
+
+func TestItem_Split_LEFT(t *testing.T) {
+
+	library := NewLibrary()
+
+	container := NewContainer()
+
+	firstID := container.NewItemWithInit(&library, ItemTypeHTML, datatype.Map{"html": "FIRST HTML ITEM"})
+
+	itemID, err := container.Execute(&library, datatype.Map{
+		"type":     "add-item",
+		"itemId":   0,
+		"itemType": "WYSIWYG",
+		"place":    "LEFT",
+		"check":    container.GetChecksum(firstID),
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "COLS", item.GetString("style"))
+		require.Equal(t, []int{1, 2}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
+}
+
+func TestItem_Split_RIGHT(t *testing.T) {
+
+	library := NewLibrary()
+
+	container := NewContainer()
+
+	firstID := container.NewItemWithInit(&library, ItemTypeHTML, datatype.Map{"html": "FIRST HTML ITEM"})
+
+	itemID, err := container.Execute(&library, datatype.Map{
+		"type":     "add-item",
+		"itemId":   0,
 		"itemType": "WYSIWYG",
 		"place":    "RIGHT",
 		"check":    container.GetChecksum(firstID),
@@ -25,4 +139,98 @@ func TestItem(t *testing.T) {
 
 	require.Nil(t, err)
 	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "COLS", item.GetString("style"))
+		require.Equal(t, []int{2, 1}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
+}
+
+func TestItem_Split_BEFORE(t *testing.T) {
+
+	library := NewLibrary()
+
+	container := NewContainer()
+
+	firstID := container.NewItemWithInit(&library, ItemTypeHTML, datatype.Map{"html": "FIRST HTML ITEM"})
+
+	itemID, err := container.Execute(&library, datatype.Map{
+		"type":     "add-item",
+		"itemId":   0,
+		"itemType": "WYSIWYG",
+		"place":    "BEFORE",
+		"check":    container.GetChecksum(firstID),
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "ROWS", item.GetString("style"))
+		require.Equal(t, []int{1, 2}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
+}
+
+func TestItem_Split_AFTER(t *testing.T) {
+
+	library := NewLibrary()
+
+	container := NewContainer()
+
+	firstID := container.NewItemWithInit(&library, ItemTypeHTML, datatype.Map{"html": "FIRST HTML ITEM"})
+
+	itemID, err := container.Execute(&library, datatype.Map{
+		"type":     "add-item",
+		"itemId":   0,
+		"itemType": "WYSIWYG",
+		"place":    "AFTER",
+		"check":    container.GetChecksum(firstID),
+	})
+
+	require.Nil(t, err)
+	require.Equal(t, 0, itemID)
+	require.Equal(t, 3, container.Len())
+
+	{
+		item := container.GetItem(0)
+		require.Equal(t, "LAYOUT", item.Type)
+		require.Equal(t, "ROWS", item.GetString("style"))
+		require.Equal(t, []int{2, 1}, item.Refs)
+	}
+	{
+		item := container.GetItem(1)
+		require.Equal(t, "WYSIWYG", item.Type)
+		require.Equal(t, "", item.GetString("html"))
+	}
+	{
+		item := container.GetItem(2)
+		require.Equal(t, "HTML", item.Type)
+		require.Equal(t, "FIRST HTML ITEM", item.GetString("html"))
+	}
 }
