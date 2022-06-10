5a33476b8b11d8eabb0b3488209b9459a1c1d2ea
Ben Pate <ben@pate.org>
2022-06-10 07:47:06 -0600

    Update action_nil.go
    be silent on unrecognized actions

--- action_nil.go
+++ action_nil.go
@@ -2,7 +2,6 @@ package nebula
 
 import (
 	"github.com/benpate/datatype"
-	"github.com/benpate/derp"
 )
 
 type NilAction datatype.Map
@@ -12,5 +11,7 @@ func (txn NilAction) Get(library *Library, container *Container, endpoint string
 }
 
 func (txn NilAction) Post(library *Library, container *Container) (int, error) {
-	return 0, derp.New(500, "content.NilAction", "Unrecognized Action Type", txn)
+
+	return 0, nil
+	// return 0, derp.New(500, "content.NilAction", "Unrecognized Action Type", txn)
 }
